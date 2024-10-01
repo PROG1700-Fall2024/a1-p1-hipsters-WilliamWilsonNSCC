528112a904ecfadcc0ac4bebbe145cdfcc24bffa
WilliamWilsonNSCC <w0457754@campus.nscc.ca>
2024-10-01 08:04:14 -0300

    Ready for marking

--- A1-P1-Hipsters.py
+++ A1-P1-Hipsters.py
@@ -8,22 +8,22 @@ Description: Assignment 1 Hipsters
 """
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
-    #Program auto rounds the cost.
+    
     welcomeMessage=print("Welcome to Hipsters Local Vinyl Records.\nWhere we keep your interests spinning faster than your record player.")
-    #customers name ex. Jimmy Hendrix
+    #customers name
     customerName=input("Hipster customer please enter your name: ")
-    #obtain cost of records purchased ex.226.19
+    #obtain cost of records purchased
     recordsPurchased=float(input("Please enter the cost of the records that you purchased today: "))
     #distance ex. 45km
     deliveryDistance=float(input("Please enter the distance for your spinning delivery: "))
 
-    #distance for delivery $15 /km (45*15=675)
+    #distance for delivery $15 /km
     deliveryDistance=deliveryDistance*15
-    #taxes records*.14 (226.19*.14=31.6666)
+    #taxes records*.14
     taxCost=recordsPurchased*.14
-    #add taxes to records purchased (226.19+31.6666=257.8566)
+    #add taxes to records purchased 
     recordCost=recordsPurchased+taxCost
-    #Add everything to get total cost (675+257.8566=932.8566)
+    #Add everything to get total cost 
     totalCost=recordCost+deliveryDistance
     print("{0}, Your Hipster's Purchase costs are: \nRecords Purchased: ${1} \nPurchase Taxes: ${2:.2f} \nDelivery Fee: ${3:.2f} \nTotal Purchase Price Including Delivery is: ${4:.2f}".format(customerName, recordsPurchased, taxCost, deliveryDistance, totalCost))
     
